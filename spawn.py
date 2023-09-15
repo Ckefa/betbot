8c525c5f7130ad6faf3255d503b008c962663738
Ckefa <ckefa65@gmail.com>
2023-09-15 12:07:23 +0300

    docker spawn automation

---
+++ spawn.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from sys import argv
+from subprocess import run
+
+
+if len(argv) > 1:
+    name = argv[0]
+    run(f"sudo docker run -d --name {name} betbot".split())
+    run(f"sudo docker cp . {name}:/app".split())
+    run(f"sudo docker restart {name}".split())
+else:
+    print("Please specify the name of your container")
