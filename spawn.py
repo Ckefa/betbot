64c3b9824acae59619bf242910d26541cefcc988
Ckefa <ckefa65@gmail.com>
2023-09-15 12:14:08 +0300

    update_spawn

--- spawn.py
+++ spawn.py
@@ -5,6 +5,7 @@ from subprocess import run
 
 if len(argv) > 1:
     name = argv[0]
+    print("Creatintg {name} constainer")
     run(f"sudo docker run -d --name {name} betbot".split())
     run(f"sudo docker cp . {name}:/app".split())
     run(f"sudo docker restart {name}".split())
