7f1e0477fdfec4771772ba30743d585143d53d50
ckefa <ckefa65@gmail.com>
2024-07-05 15:01:40 +0300

    fix

--- express/index.js
+++ express/index.js
@@ -2,16 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import path from "path"
+import { fileURLToPath } from "url";
 
 import Auth from "./utils/auth.js";
 import PlaceBet from "./utils/placebet.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 7000;
 const MONGO = process.env.MONGO || "127.0.0.1";
 const MOPORT = process.env.MOPORT || 27017;
 
-app.use(express.static(path.join(process.cwd(), "dist", "assets")));
+app.use(express.static(path.join(__dirname, "dist", "assets")));
 app.use(express.json());
 app.use(cors());
 
@@ -22,7 +26,7 @@ new Auth(app);
 new PlaceBet(app);
 
 app.get("/", async (req, resp) => {
-	resp.sendFile(path.join(process.cwd(), "dist", "index.html"))
+	resp.sendFile(path.join(__dirname, "dist", "index.html"))
 });
 
 
