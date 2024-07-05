96f14de9a9f6ba3048b0fe0ac91ce257de335bcc
ckefa <ckefa65@gmail.com>
2024-07-05 10:54:08 +0300

    fix_abolute_path_for_static_files

--- express/index.js
+++ express/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 7000;
 const MONGO = process.env.MONGO || "127.0.0.1";
 const MOPORT = process.env.MOPORT || 27017;
 
-app.use(express.static("/apps/betbot/express/dist/assets/"));
+app.use(express.static(path.join(process.cwd(), "dist", "assets")));
 app.use(express.json());
 app.use(cors());
 
@@ -22,7 +22,7 @@ new Auth(app);
 new PlaceBet(app);
 
 app.get("/", async (req, resp) => {
-	resp.sendFile("/apps/betbot/express/dist/index.html");
+	resp.sendFile(path.join(process.cwd(), "dist", "index.html"))
 });
 
 
